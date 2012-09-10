c6289df5dfdb102f5eb9440924df0adfbcae125a
Axel Beckert <abe@deuxchevaux.org>
2012-09-10 17:27:56 +0200

    Make ruthe.rb cope with non-existing stats_file

--- ruthe.rb
+++ ruthe.rb
@@ -16,9 +16,16 @@ class RutheCartoon
   end
 
   def write
+    if not File.exists?(@stats_file)
+      File.new(@stats_file, File::CREAT)
+    end
     line = "#{Time.now.tv_sec}|#{id}\n"
     last_line = last = File.open(@stats_file) { |f| f.extend(Enumerable).inject { |_,ln| ln } }
-    last_number = last_line.split("|").last.to_i
+    begin
+      last_number = last_line.split("|").last.to_i
+    rescue NoMethodError
+      last_number = 0
+    end
     File.open(@stats_file, 'a') { |f| f.write(line) } if id != last_number
   end
 
